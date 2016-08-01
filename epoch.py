69124274601448083a38228ddd4f0de370b3e6ae
ucabmrn <ucabmrn@ucl.ac.uk>
2016-08-01 19:58:18 +0100

    altered epoch to see if no_op is fucking up the base

--- epoch.py
+++ epoch.py
@@ -5,6 +5,8 @@ from itertools import chain
 import tensorflow as tf
 import threading
 from queue import Queue
+from memory_profiler import profile
+
 
 def bucket_shuffle(dict_data):
     # zip each data tuple with it's bucket id.
@@ -56,7 +58,7 @@ def run_epoch(session, models, data, training, verbose=False):
 
     return (costs / iters), (accuracy / iters)
 
-
+@profile
 def extra_epoch(session, models, data, training, verbose=False):
     """Runs the model on the given data."""
     start_time = time.time()
@@ -67,35 +69,38 @@ def extra_epoch(session, models, data, training, verbose=False):
     var_steps = 0.0
     epoch_size, id_to_data = bucket_shuffle(data)
 
-    for step in range(epoch_size):
-        (id,(x, y)) = next(id_to_data)
+    for step,(id,(x, y)) in enumerate(id_to_data):
         m = models[id]
         assert x["premise"].shape == (m.premise.get_shape())
         assert x["hypothesis"].shape == (m.hypothesis.get_shape())
 
         if training:
             eval_op = m.train_op
-        else:
-            eval_op = tf.no_op()
 
-        batch_acc, cost,act_steps, _ = session.run([m.accuracy, m.cost, m.iterations, eval_op], feed_dict={m.premise: x["premise"],
+            batch_acc, cost,act_steps, _ = session.run([m.accuracy, m.cost, m.iterations, eval_op], feed_dict={m.premise: x["premise"],
+                                      m.hypothesis: x["hypothesis"],
+                                      m.targets: y})
+        else:
+            batch_acc, cost,act_steps  = session.run([m.accuracy, m.cost, m.iterations], feed_dict={m.premise: x["premise"],
                                       m.hypothesis: x["hypothesis"],
                                       m.targets: y})
 
-        avg_steps += np.mean(act_steps)
+
+
+        avg_steps += np.sum(act_steps)
         var_steps += np.sum(np.square(act_steps))
         costs += cost
         iters += 1
         accuracy += batch_acc
-        if step % (epoch_size // 10) == 10:
-            print("%.3f acc: %.3f loss: %.3f speed: %.0f examples/s" %
+        #if step % (epoch_size // 10) == 10:
+        print("%.3f acc: %.3f loss: %.3f speed: %.0f examples/s" %
               (step * 1.0 / epoch_size,
                accuracy / iters,
                costs / iters,
                iters * m.batch_size / (time.time() - start_time)))
 
-    variance = var_steps/iters*m.batch_size - np.square(avg_steps/iters)
-    return (costs / iters), (accuracy / iters), (avg_steps/iters), variance
+    variance = var_steps/(iters*m.batch_size) - np.square(avg_steps/(iters*m.batch_size))
+    return (costs / iters), (accuracy / iters), (avg_steps/iters*m.batch_size), variance
 
 
 
